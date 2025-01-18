c267e4a24d4354ac66361734e481bff2da1dd28e
amwugrt <106160682+amwugrt@users.noreply.github.com>
2025-01-18 22:43:08 +0900

    Delete craft/gikoubu/old_hp directory

--- craft/gikoubu/old_hp/koushinrireki_files/ajs.php
+++
@@ -1,9 +0,0 @@
-﻿(function(){
-var s = "<"+"div id=\'beacon_5ca0d5423a65b279c21e5d2a029ec1c8\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
-s += "<"+"img width=\"0\" height=\"0\" src=\"https://cat.jp.as.criteo.com/delivery/lg.php?cppv=2&cpp=5qEmzXxRYi9ZTXBQZjNHK21jMEU2S25SUExteG4yREhpZTBNOGJsTXd4VVA2M1libERvdEE2Y0hTWkFJOHVSSkdXd0t2UkUxd0tMOXhyWExQcXI1UXJacTdqa0xpbzY5emhIcUgzVUNjTDVwSHM4TzFxdld1SjEwb1J6Q3Y1dDlkNWg0dlR2VmEvUG5Ec2haQWNLbjdpRDFZcm1VeWhpUDBIY3hPM2FEbjBLSFNycTRncGpYY1MxRHBlM0xMS2FLYi9ES3hieVlVV2NOdVFQeDRndVZPTE8zNytxNlJleWFBMFAvQjhpZUFmMEd2T3FHcE1lcVlSQXdDSmM4c2s0RDZtTi9HfA\"/>\n";
-s += "<"+"/div>\n";
-s += "<"+"script type=\'text/javascript\'>\n";
-s += "gCRITEO.passback();\n";
-s += "<"+"/script>\n";
-s += "\n";
-document.write(s);})();
